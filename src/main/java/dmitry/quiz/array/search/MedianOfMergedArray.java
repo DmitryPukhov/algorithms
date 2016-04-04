2e0560e3e703841b73b31d033246ff249d6621f6
Dmitry <dmitry@dmitrypukhov.pro>
2016-04-04 08:26:50 +0300

    Median of merged array refactored.

--- src/main/java/dmitry/quiz/array/search/MedianOfMergedArray.java
+++ src/main/java/dmitry/quiz/array/search/MedianOfMergedArray.java
@@ -7,6 +7,7 @@ package dmitry.quiz.array.search;
  */
 public class MedianOfMergedArray {
 
+
     /**
      * @param a Sorted array 1
      * @param b Sorted array 2
@@ -14,38 +15,33 @@ public class MedianOfMergedArray {
      */
     public static double findMedian(int[] a, int[] b) {
 
-        int i1 = 0, i2 = a.length-1, i = (i1 + i2) / 2, iPrev=-1;
-        int j1 = 0, j2 = b.length-1, j = (j1 + j2) / 2, jPrev=-1;
+        double res = -1;
 
-        // Find medians of a,b
-        while(i2 >= i1 && j2 >= j1 && (iPrev!=i || jPrev!=j )) {
+        int imin=0,imax=a.length, m = a.length, n = b.length;
 
-            if (a[i] < b[j]) {
-                // Find in left a, right b
-                i1 = Math.min(a.length - 1, i + 1);
-                j1 = Math.max(0, j - 1);
-            } else if (b[j] < a[i]) {
-                // Find in right a, left b
-                i2 = Math.max(0, i - 1);
-                j2 = Math.min(b.length - 1, j + 1);
-            } else {
-                break;
+//        Search in [imin, imax]:
+        while(true) {
+            int i = (imin + imax) / 2;
+            int j = ((m + n + 1) / 2) - i;
+            if (!(j==0 || i==m) && (b[j - 1] > a[i])) {
+                //search in[ i + 1, imax]
+                imin = i+1;
             }
-            iPrev=i;
-            jPrev=j;
-            i = (i1 + i2) / 2;
-            j = (j1 + j2) / 2;
+            else if (!(i==0 || j==n) &&(a[i - 1] > b[j])){
+                //search in[ imin, i - 1]
+                imax = i-1;
+            }else
+            //if ((m + n) is odd:
+            if ((m + n) %2 == 1){
+                //answer is max(A[i - 1], B[j - 1])
+                return Math.max(a[i - 1], b[j - 1]);
+            }
+            //if ((m + n) is even:
+            else {
+                //answer is (max(A[i - 1], B[j - 1]) + min(A[i], B[j])) / 2 return res;
+                return (Math.max(a[i - 1], b[j - 1]) + Math.min(a[i], b[j])) / 2;
+            }
         }
-
-        double res = 0;
-
-        if((a.length + b.length) % 2 == 1) {
-            res = Math.min(a[iPrev], b[jPrev]);
-        } else {
-            res =   (a[iPrev] + b[jPrev]) / 2.0;
-        }
-
-        return res;
 
     }
 
