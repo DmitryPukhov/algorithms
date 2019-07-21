3796dbaa881b19355112d6fd04688633be9e63a6
Dmitry Pukhov <dmitry.pukhov@gmail.com>
2019-07-21 07:14:00 +0300

    RouteBetweenNodes - fixed traversal for loop in graphs

--- src/main/java/dmitry/quiz/graphs/RouteBetweenNodes.java
+++ src/main/java/dmitry/quiz/graphs/RouteBetweenNodes.java
@@ -1,16 +1,22 @@
 package dmitry.quiz.graphs;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Route Between Nodes: Given a directed graph, design an algorithm to find out whether there is a
  * route between two nodes
  * Solution: use DFS (Depth First Search) from node1 until the end or reaching node2.
  **/
 public class RouteBetweenNodes {
+    private Set<Node> visited = new HashSet<>();
 
     boolean isConnected(Node<Integer> node1, Node<Integer> node2) {
         for (Node<Integer> childNode : node1.children) {
+            if(visited.contains(childNode)) continue;
             if (childNode.value == node2.value ||
                     isConnected(childNode, node2)) return true;
+            visited.add(childNode);
         }
         return false;
     }
--- src/test/java/dmitry/quiz/graphs/RouteBetweenNodesTest.java
+++ src/test/java/dmitry/quiz/graphs/RouteBetweenNodesTest.java
@@ -39,4 +39,34 @@ public class RouteBetweenNodesTest {
         assertFalse(algo.isConnected(node11, node121));
     }
 
+    @Test
+    public void isConnected_loop_false() {
+        // Create test graph
+        Node<Integer> node1 = new Node<>(1);
+        Node<Integer> node11 = new Node<>(11);
+        Node<Integer> node12 = new Node<>(12);
+        Node<Integer> node121 = new Node<>(121);
+        // Node 11 and 121 are in different branches
+        // Loop is node1 - node12 - node1
+        node1.children = new Node[]{node11, node12};
+        node12.children = new Node[]{node121, node1};
+
+        assertFalse(algo.isConnected(node11, node121));
+    }
+
+    @Test
+    public void isConnected_loop_true() {
+        // Create test graph
+        Node<Integer> node1 = new Node<>(1);
+        Node<Integer> node11 = new Node<>(11);
+        Node<Integer> node12 = new Node<>(12);
+        Node<Integer> node121 = new Node<>(121);
+        // Node 11 and 121 are in different branches
+        // Loop is node1 - node12 - node1
+        node1.children = new Node[]{node11, node12};
+        node12.children = new Node[]{node121, node1};
+
+        assertTrue(algo.isConnected(node1, node121));
+    }
+
 }
